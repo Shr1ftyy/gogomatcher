1b2340f3f22dbc1150767987cb1ed8fc44e9b61a
shr1ftyy <syeamtechdemon@gmail.com>
2024-07-27 22:12:00 +0930

    feat: insert order

--- gogomatcher/funcs.go
+++ gogomatcher/funcs.go
@@ -1,15 +1,17 @@
 package gogomatcher
 
 import (
-	// "errors"
+	"errors"
+	"math"
+	"slices"
+
 	"github.com/google/uuid"
-	// "math"
 )
 
 func NewAccount(username string) *Account {
 	account := new(Account)
 	account.uid = uuid.New()
-    account.username = username
+	account.username = username
 
 	return account
 }
@@ -18,11 +20,32 @@ func AddNewAccount(exchange *Exchange, username string) uuid.UUID {
 	account := NewAccount(username)
 	exchange.accounts[account.uid] = account
 
-    return account.uid
+	return account.uid
 }
 
 func GetAccount(exchange *Exchange, uuid uuid.UUID) *Account {
-    return exchange.accounts[uuid]
+	return exchange.accounts[uuid]
+}
+
+// type Order struct {
+// 	uid       uuid.UUID
+// 	ticker    string
+// 	price     float32
+// 	quantity  float32
+// 	side      Side
+// 	orderType OrderType
+// }
+
+func NewOrder(ticker string, price float32, quantity float32, side Side, orderType OrderType) *Order {
+    order := new(Order)
+	order.uid = uuid.New()
+	order.ticker = ticker
+	order.price = price
+	order.quantity = quantity
+	order.side = side
+	order.orderType = orderType
+
+	return order
 }
 
 func GetOrderBook(exchange Exchange, ticker string) *OrderBook {
@@ -60,51 +83,90 @@ func NewLevel(price float32) *Level {
 	return level
 }
 
-// func MatchOrder(exchange *Exchange, order *Order) (bool, error) {
-// 	orderbook := *exchange.orderbooks[order.ticker]
-// 	levelsToSearch := orderbook.asks
+func MatchOrder(exchange *Exchange, order *Order) (bool, error) {
+	orderbook := exchange.orderbooks[order.ticker]
+	levelsToSearch := &orderbook.asks
 
-// 	// for searching through bids
-// 	bidLimitCheck := func(levelPrice float32, limit float32) bool {
-// 		return levelPrice >= limit
-// 	}
+	// for searching through bids
+	bidLimitCheck := func(levelPrice float32, limit float32) bool {
+		return levelPrice >= limit
+	}
 
-// 	// for searching through asks
-// 	askLimitCheck := func(levelPrice float32, limit float32) bool {
-// 		return levelPrice <= limit
-// 	}
+	// for searching through asks
+	askLimitCheck := func(levelPrice float32, limit float32) bool {
+		return levelPrice <= limit
+	}
 
-// 	limitCheck := askLimitCheck
+	limitCheck := askLimitCheck
 
-// 	switch order.side {
-// 	case Bid:
-// 		levelsToSearch = orderbook.asks
-// 		limitCheck = askLimitCheck
-// 	case Ask:
-// 		levelsToSearch = orderbook.bids
-// 		limitCheck = bidLimitCheck
-// 	default:
-// 		return false, errors.New("not a valid order side")
-// 	}
+	switch order.side {
+	case Bid:
+		levelsToSearch = &orderbook.asks
+		limitCheck = askLimitCheck
+	case Ask:
+		levelsToSearch = &orderbook.bids
+		limitCheck = bidLimitCheck
+	default:
+		return false, errors.New("not a valid order side")
+	}
 
-// 	priceLimit := math.Inf(1)
+	priceLimit := float32(0.0) // TODO: better way to do this?
 
-// 	if order.orderType == Limit {
-// 		switch order.side {
-// 		case Bid:
-// 			priceLimit = math.MaxFloat32
-// 		case Ask:
-// 			priceLimit = 0 // TODO: can this be negative?
-// 		default:
-// 			return false, errors.New("not a valid order side")
-// 		}
-// 	}
+	if order.orderType == Limit {
+		switch order.side {
+		case Bid:
+			priceLimit = math.MaxFloat32
+		case Ask:
+			priceLimit = 0 // TODO: can this be negative?
+		default:
+			return false, errors.New("not a valid order type")
+		}
+	}
 
-// 	for idx, level := range levelsToSearch {
-// 		if !(limitCheck(level.price, priceLimit)) {
-// 			break
-// 		}
-// 	}
+	// search for level, then orders to match with
+	for _, level := range *levelsToSearch {
+		if !(limitCheck(level.price, priceLimit)) {
+			break
+		}
+	}
 
-// 	return true, nil
-// }
+	switch order.side {
+	case Bid:
+		levelsToSearch = &orderbook.bids
+	case Ask:
+		levelsToSearch = &orderbook.asks
+		limitCheck = bidLimitCheck
+	default:
+		return false, errors.New("not a valid order side")
+	}
+
+	toInsertAt := 0
+
+	// search for level to insert order into if not (fully) matched
+	for idx, level := range *levelsToSearch {
+		if level.price == order.price {
+			(*levelsToSearch)[idx].orders = append((*levelsToSearch)[idx].orders, order)
+			return true, nil
+		} else if !limitCheck(level.price, order.price) {
+			toInsertAt = idx - 1
+			break
+		}
+	}
+
+	switch order.side {
+	case Bid:
+		levelsToSearch = &orderbook.bids
+	case Ask:
+		levelsToSearch = &orderbook.asks
+		limitCheck = bidLimitCheck
+	default:
+		return false, errors.New("not a valid order side")
+	}
+
+	// appropriate price level does not exist, so we have to create it and then insert the order into it
+	newLevel := NewLevel(order.price)
+	newLevel.orders = append(newLevel.orders, order)
+	*levelsToSearch = slices.Insert(*levelsToSearch, toInsertAt, *newLevel)
+
+	return true, nil
+}
--- main.go
+++ main.go
@@ -10,10 +10,28 @@ func main() {
 	orderbook := gogomatcher.NewOrderBook("TSLA")
 	exchange := gogomatcher.NewExchange()
 
-    acctUuid := gogomatcher.AddNewAccount(exchange, "mikeburry")
-    
+	acctUuid := gogomatcher.AddNewAccount(exchange, "mikeburry")
+
 	gogomatcher.AddOrderBookToExchange(exchange, orderbook)
 	fmt.Printf("exchange: %+v\n", exchange)
 	fmt.Printf("TSLA orderbook: %+v\n", gogomatcher.GetOrderBook(*exchange, "TSLA"))
 	fmt.Printf("account: %+v\n", gogomatcher.GetAccount(exchange, acctUuid))
+
+	// new order
+	order0 := gogomatcher.NewOrder("TSLA", 69.42, 20, gogomatcher.Bid, gogomatcher.Market)
+	order1 := gogomatcher.NewOrder("TSLA", 70.42, 20, gogomatcher.Ask, gogomatcher.Market)
+
+    _, err := gogomatcher.MatchOrder(exchange, order0)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    _, err = gogomatcher.MatchOrder(exchange, order1)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+	fmt.Printf("TSLA orderbook: %+v\n", gogomatcher.GetOrderBook(*exchange, "TSLA"))
 }
